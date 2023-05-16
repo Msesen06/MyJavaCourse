27ae172411687ea858615b63f0ccb4b629c2dfd4
Mustafa sesen <sesenm0506@gmail.com>
2023-05-16 11:20:54 +0300

    acıklama

--- src/day30_Exceptions/C01_MultipleExceptions.java
+++ src/day30_Exceptions/C01_MultipleExceptions.java
@@ -1,9 +1,31 @@
 package day30_Exceptions;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class C01_MultipleExceptions {
-    public static void main(String[] args) {
-        FileInputStream fis =new FileInputStream("src/day29_Exceptions/deneme.txt");
+    public static void main(String[] args)  {
+        try {
+            FileInputStream fis =new FileInputStream("src/day29_Exceptions/deneme.txt");
+
+            int k =0;
+            while ((k= fis.read() ) != (-1)) {
+                System.out.println((char) k);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya yolu hatalı dosyaya erisilemedi");
+        } catch (IOException e) {
+            System.out.println("Dosyaya erisim ve okuma ile ilgili bir hata var");
+        }
+
+        /*
+        eger parent child iliskisi olan birden fazla exception turunu try catch ile handle etmek istersek
+            1- icice iki try catch blogu ile yapabiliriz
+            2-eger bir try ve birden fazla catch blogu olusturacaksak once child blogunu
+            yapmalıyız. cunku parent genel kapsmalıdır.
+            3- child exception icin catch yazmadan sadece parent icin bir tane catch yazabiliriz.
+         */
     }
 }
---
+++ src/day30_Exceptions/C02_BaziExceptionTurleri.java
@@ -0,0 +1,22 @@
+package day30_Exceptions;
+
+public class C02_BaziExceptionTurleri {
+    public static void main(String[] args) {
+
+        String str1 =null; //deger degil pointerdır
+
+       // System.out.println(str1.concat("java"));//NullPointerException
+
+        String a="34";//34a yazarsak parseInt() bunu sayıya ceviremez ve exception fırlatır
+        String b="55";//NumberFormatException
+
+        int sonuc =Integer.parseInt(a) + Integer.parseInt(b);
+        System.out.println(sonuc);
+
+        String str ="Java";
+        Object obj =str;
+        Integer sayi ;
+
+
+    }
+}
