65ee0e3a7c48128a238c8ed6a1042a7ed1c83b9a
Mustafa sesen <sesenm0506@gmail.com>
2023-05-16 13:45:55 +0300

    ekleme

--- src/day30_Exceptions/C04_iterator.java
+++ src/day30_Exceptions/C04_iterator.java
@@ -1,6 +1,7 @@
 package day30_Exceptions;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class C04_iterator {
@@ -24,6 +25,31 @@ public class C04_iterator {
         bugune kadar array ve list gorduk.
         collections (linkedList, queue, deque,Set)
         sonra map
+        Bu yapıların hepsi index kullanmaz
          */
+
+        //index kullanmadan list elemenlerini 2 arttirin
+
+        for (Integer each:sayilar
+             ) {
+            System.out.print(each+2 +" ");
+        }
+        System.out.println("");
+        System.out.println(sayilar);
+
+        // foreach kullanarak listin elementlerini  2 arrtırıp kaydedin
+        for (Integer each:sayilar
+             ) {
+            each+=2;
+        }
+        System.out.println(sayilar);
+        Iterator itr = sayilar.iterator();
+        itr.next();
+        itr.remove();
+        itr.next();
+        itr.remove();
+        itr.next();
+        itr.remove();
+        System.out.println(sayilar);
     }
 }
