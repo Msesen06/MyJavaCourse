0c74104de0e26069f35fec968aa2fb358940d75f
Mustafa sesen <sesenm0506@gmail.com>
2023-05-19 13:22:03 +0300

    ekleme

--- src/Practice/C03_3MayisTeamCalismasi.java
+++ src/Practice/C03_3MayisTeamCalismasi.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class C03_3MayisTeamCalismasi {
     public static void main(String[] args) {
+        //verilen metindeki unlu harfleri kaldırıp metnin yeni halini  yazdırın
         Scanner scanner =new Scanner(System.in);
         System.out.println("lütfen bir metin giriniz");
 
